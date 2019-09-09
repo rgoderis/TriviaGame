c2003607dd6d99ae1a58bf493695fd3b6d24111c
Ryan Goderis <rgoderis789@gmail.com>
2019-09-09 12:23:42 -0400

    typed out pseudo code in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,26 @@ var trivia = [ {
     correctAnswer: "Yes"
 }]
 
+// At start of game have start button visible and game div hidden
+
+// When the user hits start, the button is hidden and the game div is visible
+
+// The timer span counts down from 30
+
+// The question id displays a question from the array
+
+// the answers div displays all possibleAnswers from the array
+
+// set the clicked on aswer to be = userAnswer
+
+// check to see if userAnswer === correctAnswer
+
+// if so increase correct score and display correct answer text in question id and play gif
+
+// if incorrect increase incorrect score show incorrect answer text and play gif
+
+// if time runs out increase timesUp score show times up text and play gif
+
+// after 20 secods run a timesUp and go to next question and reset timer
+
+// after all the questions have been asked display correct score, incorrect score, and times up
